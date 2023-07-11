23c370f0685368020148f18b1a18401ace5ad405
susei <panthagadasasidhar@gmail.com>
2023-07-11 22:26:31 +0530

    generated madel_ocr.hr for predicting digits using keras

--- py files/easy_ocr.py
+++ py files/easy_ocr.py
@@ -3,55 +3,99 @@ import numpy as np
 import extractor as et
 import tensorflow 
 import keras
-import easyocr
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Flatten
+from keras.layers import Conv2D , MaxPool2D
+from keras import backend as k
+
+(X_train , y_train) , (X_test , y_test) = mnist.load_data()
+
+X_train = X_train.reshape(X_train.shape[0] , 28 , 28 , 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0] , 28 , 28 , 1).astype('float32')
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+num_classes = y_test.shape[0]
 
 
 
-
-#et.image_displayer(image)
-def prediction(diz_num, path = "BufferBox/"):
-    reader = easyocr.Reader(['ch_sim','en'])
-    image_path = path + diz_num + ".png"
-    img = cv.imread(image_path)
-    crop_img = img[7:72 , 7:72]
-    #et.image_displayer(crop_img)
-    dizit = reader.readtext(str(image_path) , detail = 0)
-    #print(dizit[0])
-    #dizit_lizt = np.empty(1 , dtype=object, order='C', like=None)
-    #dizit_lizt.insert(dizit)
-    return dizit
-
-#et.cplit_sdoku_cells("sample1.png")
-#prediction("73")
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+# normalize to range [0, 1]
+X_train = (X_train / 255.0)
+X_test = (X_test / 255.0)
 
 
-def list_dizits(image_name):
-    et.cplit_sdoku_cells(image_name)
-    dizit_lizt = np.empty((9,9), dtype=int, order='C', like=None)
-    for z in range(9):
-        for y in range(9):
-            strinp = str(z)+str(y)
-            dizit = prediction(strinp)
-            if dizit == []:
-                dizit_lizt[z][y] = 0
-            else :
-                dizit_lizt[z][y] = dizit[0]
-    print(len(dizit_lizt))
-    print(dizit_lizt)
-    return dizit_lizt
+model = Sequential()
+model.add(Conv2D(32 , (3,3) , activation='relu' , kernel_initializer= 'he_uniform' , input_shape = (28,28,1)))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
+model.add(MaxPool2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+model.save("model/madel_ocr.h5")
+
+print("Saved Model to Folder")
 
 
-def txt_fiel_io(dizit_lizt , path = "digits.txt"):
-    #nue_dizit_lizt = dizit_lizt.resize(9,9)
-    fiel = open(path , "w+")
-    queshun = str(dizit_lizt)
-    fiel.write(queshun)
-    fiel.close()
+def predict(img):
+    image = img.copy()
+
+    image = cv.resize(image, (28, 28))
+
+    image = image.astype('float32')
+    image = image.reshape(1, 28, 28, 1)
+    image /= 255
+
+    # plt.imshow(image.reshape(28, 28), cmap='Greys')
+    # plt.show()
+    model = keras.models.load_model('model/madel_ocr.h5')
+    pred = model.predict(image, batch_size=1)
+    return pred.argmax()
 
 
-dizit_lizt = list_dizits("sample1.png")
-#izzit_list_np = np.array(dizit_lizt)
-txt_fiel_io(dizit_lizt)
+def img_invartar(iimage):
+    iimage = (255-iimage)
+    
+
+imagi = "sample.png"
+doku_sels = et.cplit_b0rd_cells_np(imagi)
+
+sel = doku_sels[6]
+gray = cv.threshold(sel, 128, 255, cv.THRESH_BINARY)[1]
+
+# Find contours
+cnts = cv.findContours(gray, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+for c in cnts:
+    x, y, w, h = cv.boundingRect(c)
+    kunt_img = cv.drawContours(gray.copy() , cnts , -1 , (0,255,0) , 3)
+    cv.imshow('hi' , kunt_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows
+    if (x < 3 or y < 3 or h < 3 or w < 3):
+        # Note the number is always placed in the center
+        # Since image is 28x28
+        # the number will be in the center thus x >3 and y>3
+        # Additionally any of the external lines of the sudoku will not be thicker than 3
+        
+        ROI = gray[y:y + h, x:x + w]
+    # increasing the size of the number allws for better interpreation,
+    # try adjusting the number and you will see the differnce
+        ROI = cv.resize(ROI, (100,100))
+Rois = 255 - ROI
+cv.imshow('hi' , Rois)
+cv.waitKey(100)
+
+result = predict(Rois)
+print(result)
+
 
 
 
--- py files/extractor.py
+++ py files/extractor.py
@@ -12,6 +12,9 @@ import numpy as np
 import imageio
 import imutils
 import PIL
+import tensorflow as tf
+import keras_ocr as ko
+import matplotlib.pyplot as plt
 
 
 def image_displayer(image):
@@ -66,7 +69,7 @@ def sudoku_fienda (img_name) :
     kunts = cv.findContours(edged.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     kantors = imutils.grab_contours(kunts)
     kunt_img = cv.drawContours(base_img.copy() , kantors , -1 , (0,255,0) , 3) 
-    image_displayer(kunt_img)
+    #image_displayer(kunt_img)
     kontors = sorted(kantors,key=cv.contourArea , reverse=True) [:17 ]
     lokshn = None
 
@@ -89,30 +92,30 @@ def sudoku_fienda (img_name) :
     argument and generate 81 zy.png files for all the corresponding 
     elements of your sudoku puzzle '''
 
-def cplit_sdoku_cells(img_name):
-    board , lokshn = sudoku_fienda(img_name)
-    #cv.imwrite("Board" , board)
-    image_displayer(board)
-    length , girth = board.shape[:2]
-    print(board.shape)
-    row_start , col_start = int(0) , int(0)
-    row_end , col_end = int(length/9) , int(girth/9)
-    for ih in range(9):
-        for iw in range(9):
+# def cplit_sdoku_cells(img_name):
+#     board , lokshn = sudoku_fienda(img_name)
+#     #cv.imwrite("Board" , board)
+#     #image_displayer(board)
+#     length , girth = board.shape[:2]
+#     print(board.shape)
+#     row_start , col_start = int(0) , int(0)
+#     row_end , col_end = int(length/9) , int(girth/9)
+#     for ih in range(9):
+#         for iw in range(9):
 
-            x = int(girth/9*iw )
-            y = int(length/9*ih)
-            h = int((length / 9))
-            w = int((girth / 9 ))
-            print(x,y,h,w)
-            img = board[y:y+h, x:x+w]
-            print(img.shape)
-            #cv.imshow('hi',img)
-            #img = (255-img)
-            #cv.waitKey(50)
-            cv.imwrite("BufferBox/" + str(ih) + str(iw) + ".png" , img)
-            cv.destroyAllWindows
-    return  
+#             x = int(girth/9*iw )
+#             y = int(length/9*ih)
+#             h = int((length / 9))
+#             w = int((girth / 9 ))
+#             print(x,y,h,w)
+#             img = board[y:y+h, x:x+w]
+#             #print(img.shape)
+#             #cv.imshow('hi',img)
+#             #img = (255-img)
+#             #cv.waitKey(50)
+#             cv.imwrite("BufferBox/" + str(ih) + str(iw) + ".png" , img)
+#             cv.destroyAllWindows
+#     return  
          
 
 #cplit_sdoku_cells("sample1.png")
@@ -122,28 +125,59 @@ def cplit_sdoku_cells(img_name):
     corresponding to the name of image and develeop individual
     numpy arrays of each cell present in the sudoku board'''
 
-doku_board , lokshn = sudoku_fienda("sample1.png")
-print(doku_board.shape)
-print(doku_board[0].shape)
-print(doku_board[0].reshape(27,27))
-print(doku_board[0].shape)
-imagi = "sample1.png"
+# doku_board , lokshn = sudoku_fienda("sample1.png")
+# print(doku_board.shape)
+# print(doku_board[0].shape)
+# print(doku_board[0].reshape(27,27))
+# print(doku_board[0].shape)
+
 def cplit_b0rd_cells_np(imag_nem):
-    doku_board , lokshn = sudoku_fienda(imagi)
+    doku_board , lokshn = sudoku_fienda(imag_nem)
     sels = []
     roes = np.vsplit(doku_board,9)
     for z in roes:
         cals = np.hsplit(z,9)
         for sel in cals:
             sel = sel[7:70 , 7:70]
-            sel = cv.resize(sel , (27,27) )/255.0
-            cv.imshow('sels' , sel)
-            cv.waitKey(50)
+            #sel = (sel)/255.0
+            #cv.imshow('sels' , sel)
+            #cv.waitKey(50)
             sels.append(sel)
 
     return sels
 
-cplit_b0rd_cells_np(imagi)
+
+
+
+
+
+
+
+'''
+    Predicshun : following function tries to predict the 
+    digits from the numpy array directly using 
+    keras model model_ocr.h5 instead of using easy_ocr 
+    which took in images previously'''
+
+# def predicshun_of_npsels(nparr_nem):
+#     pipline = ko.pipeline.Pipeline()
+#     boku_sels = cplit_b0rd_cells_np(imagi)
+#     doku_zels = np.array(boku_sels).reshape(-1 , 9 , 9 , 1)
+#     classes = np.arange(0,10)
+#     model = tf.keras.models.load_model("model/digits.h5")
+
+#     predctd_nems = []
+#     for k in predctd_nems:
+#         index = (np.argmax(k))
+#         predctd_nem = classes[index]
+#         predctd_nems.append(predctd_nem)
+#     print(predctd_nems)
+
+#     return
+
+
+# predicshun_of_npsels(imagi)
+
 
 
 
